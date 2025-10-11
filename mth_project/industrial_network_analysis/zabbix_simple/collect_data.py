61d9c666c39f6723954ab9e85b69c6e1b9ef5a23
Julia Slazak <julia.slazak@valmet.com>
2025-10-11 12:35:01 +0300

    trying to resolve issues with no data downlaoded

--- mth_project/industrial_network_analysis/zabbix_simple/collect_data.py
+++ mth_project/industrial_network_analysis/zabbix_simple/collect_data.py
@@ -93,7 +93,7 @@ class ZabbixDataCollector:
                 print(f"DEBUG FOR DATA COLLECTION: {item_name_lower}, {item_key_lower}")
                 # Check if any search criteria matches
                 for criteria in self.search_criteria:
-                    print(f"DEBUG DATA COLELCTION: {criteria}")
+                    print(f"DEBUG DATA COLLECTION: {criteria}")
                     if (criteria.lower() in item_name_lower or 
                         criteria.lower() in item_key_lower):
                         if item['hostid'] in host_lookup:
